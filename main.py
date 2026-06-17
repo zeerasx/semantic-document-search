9465d6da35ef36776b48658f49a4d5e3dcade059
zeeras <zeeras.dev@gmail.com>
2026-06-17 11:40:12 +0500

    refactor: main to use indexing pipeline

--- main.py
+++ main.py
@@ -3,45 +3,14 @@ from src.chunker import TextChunker
 from src.embedder import Embedder
 from src.search import SemanticSearcher
 from src.vector_store import VectorStore
+from src.pipeline import IndexingPipeline
 
 def main():
-    # Ingestion
-    pdf_path = "data/pdfs/DeepLearningforDummies.pdf"
-    loader = PDFLoader(pdf_path)
-    #text = loader.extract_text()
-    pages = loader.extract_pages()
 
-    #Chunking
-    print("////////////////////////////CHUNKING/////////////////////////////////////////////////////////")
-    chunker = TextChunker(
-        chunk_size=500,
-        overlap=100
-    )
-
-    chunks = chunker.chunk_pages(pages, document_name="DeepLearningforDummies.pdf")
-
-    print(
-        f"Total Chunks: {len(chunks)}"
-    )
-
-    for chunk in chunks[:3]:
-        print("======================================")
-        print(chunk["chunk_id"])
-        print("======================================")
-        print(chunk["chunk"])
-
-    # Embedding
-    print("///////////////////////////EMBEDDING//////////////////////////////////////////////////////////")
-    embedder = Embedder()
-    print("Embedding Chunks...")
-    embeddings = embedder.embed_chunks(chunks)
-    print("Embeddings Generated.")
-    print(f"Total Embeddings: {len(embeddings)}")
-    print(f"Embedding Dimension: {embeddings[0].shape}")
-    print("Sample Embedding:")
-    print(embeddings[0][:5])
-
-    # Semantic Search
+    # Initialize the indexing pipeline
+    pdf_directory = "data/pdfs/"
+    pipeline = IndexingPipeline(pdf_directory=pdf_directory)
+    (chunks, embeddings, embedder, vector_store) = pipeline.build()
 
     ## Making query and its embedding
     query = input( "Enter search query: ")
